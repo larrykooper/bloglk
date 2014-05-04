04c63f19f3d55727210b89fef33c995a83e4f198
larrykooper <larry.kooper@gmail.com>
2014-05-03 21:33:53 -0400

    initial commit

---
+++ routes/entries.js
@@ -0,0 +1,33 @@
+var Entry = require('../lib/entry');
+
+exports.list = function(req, res, next) {
+    // Retrieve entries
+    Entry.getRange(0, -1, function(err, entries) {
+        if (err) return next(err);
+        
+        // Render HTTP response 
+        res.render('entries', {
+            title: 'Entries',
+            entries: entries,
+        });
+         
+    });
+};
+
+exports.form = function(req, res) {
+    res.render('post', {title:'Post'});
+};
+
+exports.submit = function(req, res, next) {
+    var data = req.body.entry; 
+    
+    var entry = new Entry({
+        "title": data.title,
+        "body": data.body
+    });
+    
+    entry.save(function(err) {
+        if (err) return next(err);
+        res.redirect('/');
+    });
+};
